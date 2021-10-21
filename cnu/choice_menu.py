f01f929cfb078bcc0d080e2c3e669a5467d145d7
main_kiosk <https://github.com/Hyunnys>
2021-10-21 16:16:13 +0900

    DAUM 뉴스 제목 웹크롤링

---
+++ cnu/choice_menu.py
@@ -0,0 +1,18 @@
+# 1. 햄버거 단품 메뉴 선택하는 메서드
+def choice_burger():
+    pass
+
+
+
+# 2. 사이드 단품 메뉴 선택하는 메서드
+def choice_side():
+    pass
+
+
+
+# 3. 음료 단품 메뉴 선택하는 메서드
+def choice_drink():
+    pass
+
+
+
